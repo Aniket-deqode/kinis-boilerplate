6c78b8d732c1ea769bd6b4bf35f9f105b3665101
Aniket-deqode <atikone@deqode.com>
2021-08-17 21:54:37 +0530

    failed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'with husky4 lint c test!';
+    return 'with husky4 & lint c test!';
   }
 }
